6aca08ee3e45daf834a9fd04f3c415f72a6fd6b4
Priscila de Araujo Caimi <paraujocaimi@hotmail.com>
2020-06-03 20:51:57 -0300

    Conteudo da primeira aula

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 cypressFrontEnd
+
+Projeto criado para estudar a aplicabilidade do cypress para testes web.
+
+### Serviço utilizado para os testes: 
+https://github.com/samlucax/youtube-cypress 
+
+### Frameworks utilizados nos testes
+
---
+++ cypress/integration/ongs.spec.js
@@ -0,0 +1,38 @@
+/// <reference types="cypress"/>
+
+describe('Ongs', () => {
+
+    it.skip("devem poder realizar um cadastro", ()=>{
+        cy.visit('http://localhost:3000/register')
+        // cy.get busca um elemento 
+        // .type() insere o elemento
+        cy.get('[data-cy="name"]').type('Dogs queridos')
+        cy.get('[data-cy="email"]').type('dogs@email.com')
+        cy.get('[data-cy="whatsapp"]').type('51997440272')
+        cy.get('[data-cy="city"]').type('Porto Alegre')
+        cy.get('[data-cy="uf"]').type('RS')
+
+        //routing
+        //start server com cy.server()
+        cy.server()
+
+        // criar uma rota com cy.route()
+        //atribuir rota a um alias
+        cy.route('POST', '**/ongs').as('postOng');
+        cy.get('[data-cy=submit]').click();
+
+        //esperar com cy.wait
+        cy.wait('@postOng').then((xhr) => {
+            expect(xhr.status).be.eq(200);
+            expect(xhr.response.body).has.property('id');
+            expect(xhr.response.body.id).is.not.null;
+        });
+
+    });
+
+    it("deve poder realizar um login no sistema",()=>{
+        cy.visit('http://localhost:3000')
+        cy.get('input').type(Cypress.env('createdOngId'))
+        cy.get('.button').click()
+    })
+})
